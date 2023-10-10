0e7b1d0475f043c68d6b474fd0a62a5d970743b5
febripram <febripramana0@gmail.com>
2023-10-10 19:17:39 +0700

    tugas-binar

---
+++ scripts/sortCarByYearAscendingly.js
@@ -0,0 +1,14 @@
+function sortCarByYearAscendingly(cars) {
+  // Sangat dianjurkan untuk console.log semua hal hehe
+  console.log(cars);
+
+  // Clone array untuk menghindari side-effect
+  // Apa itu side effect?
+  const result = [...cars];
+
+  // Tulis code-mu disini
+  const carsSortedByYearAsc = result.sort((a, b) => a.year - b.year);
+  console.log(carsSortedByYearAsc);
+  // Rubah code ini dengan array hasil sorting secara ascending
+  return result;
+}
